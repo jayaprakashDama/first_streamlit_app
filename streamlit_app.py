a76a00a8229e6a9451231898f3d10e1e77cea64e
jayaprakashDama <119881459+jayaprakashDama@users.noreply.github.com>
2023-10-18 09:46:00 -0500

    Update information in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
+import pandas as pd
 
-streamlit.title('My first stream lit application')
+streamlit.title('My Breakfast list')
+streamlit.header('Having breakfast with brother')
+streamlit.text('list of breakfast items')
+
+my_fruits= pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+streamlit.Dataframe(my_fruits)

8c5be56078d1c6015f33eee91ff37108441f3587
jayaprakashDama <119881459+jayaprakashDama@users.noreply.github.com>
2023-10-18 16:43:52 -0500

    Update streamlit_app.py response as json

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ streamlit.header('Having breakfast with brother')
 streamlit.text('list of breakfast items')
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
 
 my_fruits= pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 streamlit.multiselect("list of fruits in drink:", list(my_fruits.Fruit))

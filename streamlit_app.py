c5787b979e321fdcd3583e0e8cbfd941c87909b9
jayaprakashDama <119881459+jayaprakashDama@users.noreply.github.com>
2023-10-18 09:50:42 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,4 +7,4 @@ streamlit.text('list of breakfast items')
 
 my_fruits= pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 streamlit.dataframe(my_fruits)
-streamli.multiselect("list of fruits in drink:", list(my_fruits.index))
+streamlit.multiselect("list of fruits in drink:", list(my_fruits.index))

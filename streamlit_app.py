6256a6ffe53fc8cdb06bc96a510e4735c1a9b428
jayaprakashDama <119881459+jayaprakashDama@users.noreply.github.com>
2023-10-18 16:45:08 -0500

    Update streamlit_app.py with json_normalize

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,9 @@ streamlit.text('list of breakfast items')
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
+streamlit.dataframe(fruityvice_normalized)
 
 my_fruits= pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 streamlit.multiselect("list of fruits in drink:", list(my_fruits.Fruit))

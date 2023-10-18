31db4a86401bc77672efbd093c4c6cd867511029
jayaprakashDama <119881459+jayaprakashDama@users.noreply.github.com>
2023-10-18 16:42:31 -0500

    Update streamlit_app.py with requests package

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,13 @@
 import streamlit
 import pandas as pd
+import requests
 
 streamlit.title('My Breakfast list')
 streamlit.header('Having breakfast with brother')
 streamlit.text('list of breakfast items')
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 
 my_fruits= pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 streamlit.multiselect("list of fruits in drink:", list(my_fruits.Fruit))
